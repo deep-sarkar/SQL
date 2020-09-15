f4328c212b7e1c34e9c96bc0e7f2f7eaedcd7a84
deep-sarkar <deepsarkar96@gmail.com>
2020-09-15 17:50:18 +0530

    [Deep]Add.added table creation and data copy from csv queries

---
+++ CPU/cpu_log.sql
@@ -0,0 +1,53 @@
+-- Create database
+CREATE TABLE temporary_MIS(
+Date_Time DATE NOT NULL, 
+Cpu_Count INT NOT NULL,
+Cpu_Working_Time DECIMAL(11,3) NOT NULL,
+Cpu_idle_Time DECIMAL(11,3) NOT NULL,
+cpu_percent DECIMAL(11,3) NOT NULL,
+Usage_cpu_count INT NOT NULL,
+number_of_software_interrupts_since_boot DECIMAL(11,3) NOT NULL,
+number_of_system_calls_since_boot INT NOT NULL,
+number_of_interrupts_since_boot INT NOT NULL,
+cpu_avg_load_over_1_min DECIMAL(11,3) NOT NULL,
+cpu_avg_load_over_5_min DECIMAL(11,3) NOT NULL,
+cpu_avg_load_over_15_min DECIMAL(11,3) NOT NULL,
+system_total_memory BIGINT NOT NULL,
+system_used_memory BIGINT NOT NULL,
+system_free_memory BIGINT NOT NULL,
+system_active_memory BIGINT NOT NULL,
+system_inactive_memory BIGINT NOT NULL,
+system_buffers_memory BIGINT NOT NULL,
+system_cached_memory BIGINT NOT NULL,
+system_shared_memory BIGINT NOT NULL,
+system_avalible_memory BIGINT NOT NULL,
+disk_total_memory BIGINT NOT NULL,
+disk_used_memory BIGINT NOT NULL,
+disk_free_memory BIGINT NOT NULL,
+disk_read_count BIGINT NOT NULL,
+disk_write_count BIGINT NOT NULL,
+disk_read_bytes BIGINT NOT NULL,
+disk_write_bytes BIGINT NOT NULL,
+time_spent_reading_from_disk BIGINT NOT NULL,
+time_spent_writing_to_disk BIGINT NOT NULL,
+time_spent_doing_actual_Input_Output BIGINT NOT NULL,
+number_of_bytes_sent BIGINT NOT NULL,
+number_of_bytes_received BIGINT NOT NULL,
+number_of_packets_sent BIGINT NOT NULL,
+number_of_packets_recived BIGINT NOT NULL,
+total_number_of_errors_while_receiving BIGINT NOT NULL,
+total_number_of_errors_while_sending BIGINT NOT NULL,
+total_number_of_incoming_packets_which_were_dropped BIGINT NOT NULL,
+total_number_of_outgoing_packets_which_were_dropped BIGINT NOT NULL,
+boot_time varchar(100) NOT NULL,
+user_name varchar(50) NOT NULL,
+keyboard DECIMAL NOT NULL,
+mouse DECIMAL NOT NULL,
+technology varchar(100) NOT NULL,
+files_changed INT NOT NULL
+); 
+
+-- copy csv data to table
+COPY cpu_analysis."temporary_mis" FROM '/home/deep/Documents/csv/CpuLogData2019-09-18.csv'
+DELIMITER ','
+CSV HEADER;
