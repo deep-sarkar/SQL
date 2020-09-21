8291a51dc637b8146e54f025f580beae2a4ebfe9
deep-sarkar <deepsarkar96@gmail.com>
2020-09-21 10:54:25 +0530

    [Deep]Add.added joins with cte

--- practice/cte.sql
+++ practice/cte.sql
@@ -13,3 +13,20 @@ SELECT user_name, count(user_name) as attandance
 FROM cte_attandance
 GROUP BY user_name;
 
+-- WITH cte we can perform join and subquery at same time
+-- Display attandance with maximum spent time with writting disk for perticular user
+WITH cte_attandance AS (
+SELECT date_time::DATE, user_name
+FROM temporary_mis
+GROUP BY 1,2),
+
+cte_disk_writing AS (
+SELECT user_name,max(time_spent_writing_to_disk) as max_spent_time
+FROM temporary_mis
+GROUP BY user_name)
+
+SELECT cte_attandance.user_name, count(cte_attandance.user_name) as attandance, cte_disk_writing.max_spent_time
+FROM cte_attandance
+INNER JOIN cte_disk_writing 
+ON cte_disk_writing.user_name = cte_attandance.user_name
+GROUP BY cte_attandance.user_name, cte_disk_writing.max_spent_time;
